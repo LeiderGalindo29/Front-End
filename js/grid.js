e6f3e6ceecae8d6a1f47ac0c7aa567b393bf2a75
LeiderGalindo29 <104436225+LeiderGalindo29@users.noreply.github.com>
2022-08-10 22:17:34 -0500

    10/08/2022

---
+++ js/grid.js
@@ -0,0 +1,9 @@
+'use strick'
+const icono = document.querySelector('.hamb')
+
+
+const menu = document.querySelector('.menu')
+
+icono.addEventListener("click", ()=>{
+      menu.classList.toggle("active")
+})
